bb7f9f24cf22fba232bd76f83cce0c546d9f7f0e
RenaissanceIOS <prostopochta0420@gmail.comprostopochta0420@gmail.comprostopochta0420@gmail.comprostopochta0420@gmail.comprostopochta0420@gmail.comprostopochta0420@gmail.comprostopochta0420@gmail.comprostopochta0420@gmail.com>
2022-02-08 13:08:22 +0200

    Initial Commit

---
+++ App Product1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Product1
+//
+//  Created by Andrey Pavlenko on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
